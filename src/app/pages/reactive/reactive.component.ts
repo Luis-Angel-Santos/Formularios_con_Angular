2b2e3f414b9dea6cd1ad0c93927e045f9f4b746a
Luis Angel <santosangel748@gmail.com>
2020-11-11 16:40:54 -0600

    Validar que password1 sea igual al password2

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -38,17 +38,31 @@ export class ReactiveComponent implements OnInit {
   get ciudadNoValido(){
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
   }
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched
+  }
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return (pass1 === pass2 ) ? false : true;
+  }
+
 
   crearFormulario(){
     this.forma = this.fb.group({
       nombre:   ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(3), this.validadores.noHerrera]],
       correo:   ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      pass1:    ['', Validators.required],
+      pass2:    ['', Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad:   ['', Validators.required]
       }),
       pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1', 'pass2')
     });
   }
 
